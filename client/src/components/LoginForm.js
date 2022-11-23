a66f7a36f79c571a800353f2e80ae862163993bb
DylanGodfrey <39961099+DylanGodfrey@users.noreply.github.com>
2022-11-22 21:18:16 -0500

    Login Form converted

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -6,6 +6,7 @@ import { loginUser } from '../utils/API';
 import Auth from '../utils/auth';
 
 const LoginForm = () => {
+  // Set intial states
   const [userFormData, setUserFormData] = useState({ email: '', password: '' });
   const [validated] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
@@ -26,25 +27,23 @@ const LoginForm = () => {
     }
 
     try {
-      const response = await loginUser(userFormData);
-
-      if (!response.ok) {
-        throw new Error('something went wrong!');
+      // Use form data to try to login
+      const {data} = await loginUser({
+        variables: {...userFormData}
+      });
+      const {token, user} = data;
+      // Login
+      Auth.login(data.login.token);
+      } catch (err) {
+          console.error(err);
+          setShowAlert(true);
       }
-
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
-    } catch (err) {
-      console.error(err);
-      setShowAlert(true);
-    }
-
-    setUserFormData({
-      username: '',
-      email: '',
-      password: '',
-    });
+      // Reset form data after login
+      setUserFormData({
+          username: '',
+          email: '',
+          password: '',
+      });
   };
 
   return (
